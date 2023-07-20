ed3195c3651c7e32f3c1b729bc27c0f02cd07afb
Ashish <kharbashish2121@gmail.com>
2023-07-20 21:52:16 +0530

    changed firebase database location to US to debug database error

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -3,14 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-    apiKey: 'AIzaSyAwIz877ZHrbPsD40DF9Dh8iTClzcAQIzs',
-    authDomain: 'chat-on-31c9e.firebaseapp.com',
-    projectId: 'chat-on-31c9e',
-    storageBucket: 'chat-on-31c9e.appspot.com',
-    messagingSenderId: '900681222453',
-    appId: '1:900681222453:web:fc55b9a5a2a86d999e247c',
-
-};
+    apiKey: "AIzaSyASo0uvDIQ7TSqYQgZk1c9_-wv1DeT9g7Q",
+    authDomain: "chat-on-5e89d.firebaseapp.com",
+    projectId: "chat-on-5e89d",
+    storageBucket: "chat-on-5e89d.appspot.com",
+    messagingSenderId: "704116147255",
+    appId: "1:704116147255:web:006717f6a317af03e8ebb2"
+  };
 
 const app = firebase.initializeApp(config);
 
