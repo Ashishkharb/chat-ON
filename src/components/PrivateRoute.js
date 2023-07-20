ea655b2faf54675e147104d9e09cfd0fd5fbbc7e
Ashish <kharbashish2121@gmail.com>
2023-07-20 22:53:26 +0530

    global profile state management with context: put real time subs

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,25 @@
 import { Redirect, Route } from 'react-router-dom';
+import { Container, Loader } from 'rsuite';
 import { useProfile } from '../context/profile.context';
 
 const PrivateRoute = ({ children, ...routeProps }) => {
-    const profile = useProfile();
-    
-    if (!profile) {
-        return <Redirect to="/signin"/>;
+    const { isLoading, profile } = useProfile();
+
+    if (isLoading && !profile) {
+        return (
+            <Container>
+                <Loader
+                    center
+                    vertical
+                    size="md"
+                    content="Loading"
+                    speed="slow"
+                />
+            </Container>
+        );
+    }
+    if (!profile && !isLoading) {
+        return <Redirect to="/signin" />;
     }
     return <Route {...routeProps}>{children}</Route>;
 };
--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,11 +1,25 @@
 import { Redirect, Route } from 'react-router-dom';
+import { Container, Loader } from 'rsuite';
 import { useProfile } from '../context/profile.context';
 
 const PublicRoute = ({ children, ...routeProps }) => {
-    const profile = useProfile();
+    const {isLoading,profile} = useProfile();
 
-    if (profile) {
-        return <Redirect to="/" />;
+    if (isLoading && !profile) {
+        return (
+            <Container>
+                <Loader
+                    center
+                    vertical
+                    size="md"
+                    content="Loading"
+                    speed="slow"
+                />
+            </Container>
+        );
+    }
+    if (profile && !isLoading) {
+        return <Redirect to="/signin" />;
     }
     return <Route {...routeProps}>{children}</Route>;
 };
